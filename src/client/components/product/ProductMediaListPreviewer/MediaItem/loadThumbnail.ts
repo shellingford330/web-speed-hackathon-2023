84848f87da5c7e1ea3c2d3105716e9c6fbb52a17
shellingford330 <pg.econ4869@gmail.com>
2023-03-04 21:42:10 +0900

    feat: prepare thumbnails

--- src/client/components/product/ProductMediaListPreviewer/MediaItem/loadThumbnail.ts
+++ src/client/components/product/ProductMediaListPreviewer/MediaItem/loadThumbnail.ts
@@ -1,23 +1,11 @@
+import { getMediaType } from "../../../../utils/get_media_type";
+
 export const loadThumbnail = async (url: string) => {
-  const video = document.createElement('video');
+  const mediaType = getMediaType(url);
 
-  await new Promise((resolve) => {
-    video.src = url;
-    video.addEventListener('canplaythrough', resolve, { once: true });
-  });
-
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
-
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  if (context === null) {
-    return;
+  if (mediaType == 'image') {
+    return `/thumbnails${url}`
   }
 
-  video.currentTime = 0;
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-  const thumbnailData = canvas.toDataURL('image/png');
-  return thumbnailData;
+  return `/thumbnails${url.replace('webm', 'avif')}`;
 };
